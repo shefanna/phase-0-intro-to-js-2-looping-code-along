eb751a8bd630613afe79ed186c5074c3176cbf90
shefinannathampy <shefin04annathampy94@gmail.com>
2023-03-10 20:11:39 -0500

    looping lab completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const greetings = [];
+    for (let x = 0; x < names.length; x++) {
+        greetings.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
+    }
+    return greetings;
+    }
+    function countDown(intNumber) {
+        for (let a = intNumber; a >= 0; a--) {
+          console.log(a);
+        }
+      }
